d06c43a137bf69d6ec952a533537a16a4ead8540
krd16 <230078@iessanalberto.com>
2025-12-15 09:48:45 +0100

    java program fixed

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         while (contador <= 10) {
             System.out.println(numero + " x " + contador + " = " + (numero * contador));
-
+            contador++;
         }
 
     }
